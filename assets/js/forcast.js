fbc695ad72288df2b3c79c2fee7ab9fb4e1e6677
mherring11 <m.herring11@yahoo.com>
2022-06-12 10:53:19 -0500

    added icons

---
+++ assets/js/forcast.js
@@ -0,0 +1,23 @@
+const key = "3mV38vGw1p0Y1pVreLJIYvwpnCkmUWy3";
+
+//
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
